3253e043b5ec4bfec49b485e626aab42b1a542fe
abillig <avrambillig@gmail.com>
2016-06-27 17:03:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
 class App < Sinatra::Base
 
+
+
+  get '/hello' do 
+    erb :hello
+  end
+
 	get '/' do
 		erb :index
 	end
+
+  get '/goodbye' do 
+    @name = "Joe"
+    erb :goodbye
+  end
+
+  get '/date' do 
+    @time = Time.now
+    erb :date
+  end
 
 
 end
